8303adeb128520fe6537f708ff7ff0dcc10929f9
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-11-25 09:11:46 +0900

    SECRET_KEYを環境変数からとる

--- FairWindPortal/settings.py
+++ FairWindPortal/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# managed in local_settings.py
+# managed in local_settings.py in development
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
