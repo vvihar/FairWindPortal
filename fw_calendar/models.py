e854a9644ce4bb78d922bf4a0d9aaedba05e0d3b
Gulmohar <92192914+vvihar@users.noreply.github.com>
2022-12-08 20:16:36 +0900

    migrate

--- fw_calendar/models.py
+++ fw_calendar/models.py
@@ -1,21 +1,17 @@
-from datetime import datetime, timedelta
+import datetime
 
 from django.db import models
 from django.utils import timezone
 
 
+# Create your models here.
 class Schedule(models.Model):
     """スケジュール"""
 
     summary = models.CharField("概要", max_length=50)
     description = models.TextField("詳細な説明", blank=True)
-    start_time = models.TimeField(
-        "開始時間", default=datetime.strftime(datetime.now(), "%H:%M")
-    )
-    end_time = models.TimeField(
-        "終了時間",
-        default=datetime.strftime(datetime.now() + timedelta(hours=1), "%H:%M"),
-    )
+    start_time = models.TimeField("開始時間", default=datetime.time(7, 0, 0))
+    end_time = models.TimeField("終了時間", default=datetime.time(7, 0, 0))
     date = models.DateField("日付")
     created_at = models.DateTimeField("作成日", default=timezone.now)
 
